9cc87fd85a5f760b24e4ad224bb509d3a14c350d
pinkkila <petteri.pinkkila@outlook.com>
2024-11-07 23:32:43 +0200

    Add setting-page and routing

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,8 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Home Page</Text>
-      <Link href="/settings">Got to Settings</Link>
+      <Text>settings</Text>
     </View>
   );
 }
