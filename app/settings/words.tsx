fe55f9141bf3942dfeb7ed5b76e58c1edc5b84cb
pinkkila <petteri.pinkkila@outlook.com>
2024-11-30 15:22:57 +0200

    Add initial Your Words list style

--- app/settings/words.tsx
+++ app/settings/words.tsx
@@ -2,25 +2,39 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 import { useWords } from "@/hooks/useWords";
 
 export default function Words() {
-  const {words, handleDelete } = useWords();
+  const { words, handleDelete } = useWords();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Words</Text>
       <FlatList
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.itemContainer}>
-            <Text style={styles.text}>{item.english}</Text>
-            <Text style={styles.text}>{item.finnish} </Text>
-            <Text style={styles.text}>{item.correct} </Text>
-            <Text style={styles.text}>{item.wrong} </Text>
-            <Text
-              style={{ color: "#0000ff" }}
-              onPress={() => handleDelete(item.id)}
-            >
-              Delete
-            </Text>
+            <View style={styles.itemHeader}>
+              <Text style={styles.wordText}>{item.english}</Text>
+              <Text style={styles.wordText}>{item.finnish}</Text>
+            </View>
+
+            <View style={styles.itemBottom}>
+              <View style={styles.itemDetails}>
+                <Text style={styles.text}>Correct: {item.correct}</Text>
+                <Text style={styles.text}>Wrong: {item.wrong}</Text>
+              </View>
+
+              <Text
+                style={styles.updateText}
+                onPress={() => handleDelete(item.id)}
+              >
+                Update
+              </Text>
+
+              <Text
+                style={styles.deleteText}
+                onPress={() => handleDelete(item.id)}
+              >
+                Delete
+              </Text>
+            </View>
           </View>
         )}
         data={words}
@@ -32,17 +46,56 @@ export default function Words() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    padding: 10,
+  },
+
+  itemContainer: {
+    flexDirection: "column", 
+    backgroundColor: "#fff", 
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 8,
+  },
+
+  itemHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 8,
+  },
+
+  itemBottom: {
+    marginTop: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  itemDetails: {
+    marginTop: 5,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+
+  wordText: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#333",
   },
   text: {
-    fontSize: 16,
-    lineHeight: 24,
+    fontSize: 15,
     fontWeight: "600",
-    color: "white",
+    color: "#333",
   },
-  itemContainer: {
-    flexDirection: "row",
-    margin: 5,
+  updateText: {
+    color: "#ff4d4d",
+    fontWeight: "bold",
+    marginTop: 10,
+    alignSelf: "flex-end",
+  },
+  deleteText: {
+    color: "#ff4d4d",
+    fontWeight: "bold",
+    marginTop: 10,
+    alignSelf: "flex-end",
   },
 });
